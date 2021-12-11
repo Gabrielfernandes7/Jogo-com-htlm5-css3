ecdc7043a8f9e035a89a69a49889bc809989959c
Gabrielfernandes7 <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2021-12-11 16:18:08 -0300

    funcao movejogador

--- js/script.js
+++ js/script.js
@@ -7,22 +7,6 @@ function start(){
     $("fundoGame").append("<div id='inimigo2'></div>");
     $("fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-    //Principais variáveis do jogo
-    var jogo = {};
-    //GAME LOOP
-    jogo.timer = setInterval(loop, 30);
-
-    function loop() {
-        movefundo();
-    }
-
-    //Função que movimenta o jogo
-
-    function movefundo() {
-        esquerda = parseInt($("#fundoGame").css("background-position"));
-        $("fundoGame").css("background-position", esquerda - 1);
-    }//Move fundo
-
     var TECLA = {
         W: 87,
         S: 83,
@@ -34,7 +18,60 @@ function start(){
     //Verifica se o usuario pressionou alguma tecla
 
     $(document).keyup(function (e) {
+        jogo.pressionou[e.wich] = true;
+    });
+
+    $(document).keydown(function (e) {
         jogo.pressionou[e.wich] = false;
     });
+
+    //Principais variáveis do jogo
+    var jogo = {};
+    
+
+    //GAME LOOP
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+        movefundo();
+        movejogador();
+    }
+
+    //Função que movimenta o jogo
+
+    function movefundo() {
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("fundoGame").css("background-position", esquerda - 1);
+    }//Move fundo
+
+
+    function movejogador() {
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);
+
+            if (topo <= 0) {
+                $("#jogador").css("top", topo + 10);
+            }
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo + 10);
+
+            if (topo >= 434) {
+                $("jogador").css("top", topo - 10);
+            }
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+            //Chama a função disparo
+        }
+    }
+
+    
+
+   
+
 
 } // fim da função
