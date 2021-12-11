51c2b40525691d7eda76f01971f4006ef243e9ef
Gabrielfernandes7 <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2021-12-10 23:32:52 -0300

    criado pasta css e js, modificao do index

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function start(){
+    //Inicio do jogo
+    $("#inicio").hide();
+
+    $("fundoGame").append("<div id='jogador'></div>");
+    $("fundoGame").append("<div id='inimigo1'></div>");
+    $("fundoGame").append("<div id='inimigo2'></div>");
+    $("fundoGame").append("<div id='amigo'></div>");
+} // fim da função
