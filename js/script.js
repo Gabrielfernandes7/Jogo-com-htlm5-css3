49a90529d47256cb7789698d2c501eb86a902496
Gabrielfernandes7 <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2021-12-11 00:21:35 -0300

    modificacoes nos arquivos script.js e style.css

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@ function start(){
     //Inicio do jogo
     $("#inicio").hide();
 
-    $("fundoGame").append("<div id='jogador'></div>");
-    $("fundoGame").append("<div id='inimigo1'></div>");
+    $("fundoGame").append("<div id='jogador' class='anima1></div>");
+    $("fundoGame").append("<div id='inimigo1' class='anima2'></div>");
     $("fundoGame").append("<div id='inimigo2'></div>");
-    $("fundoGame").append("<div id='amigo'></div>");
+    $("fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+    //Principais variáveis do jogo
+    var jogo = {};
+    //GAME LOOP
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+        movefundo()
+    }
 } // fim da função
